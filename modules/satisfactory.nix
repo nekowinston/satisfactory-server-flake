58196fd3679fc23551863410f3ec2c4b9839e6fb
winston <hey@winston.sh>
2025-11-02 22:37:12 +0100

    feat: add messagingPort, allow arbitrary ini config
    also allow multihome config instead of enforcing DisableRouting

--- modules/satisfactory.nix
+++ modules/satisfactory.nix
@@ -7,6 +7,65 @@
 }:
 let
   cfg = config.services.satisfactory;
+
+  inherit (lib) mapAttrsToList types;
+
+  iniFromSettings = {
+    Engine = {
+      "/Script/Engine.Engine" = {
+        NetClientTicksPerSecond = cfg.settings.maxTickrate;
+      };
+      "/Script/Engine.GarbageCollectionSettings" = {
+        "gc.MaxObjectsInEditor" = cfg.settings.maxObjects;
+      };
+      "/Script/FactoryGame.FGSaveSession" = {
+        mNumRotatingAutosaves = cfg.settings.autosaveNumber;
+      };
+      "/Script/OnlineSubsystemUtils.IpNetDriver" = {
+        ConnectionTimeout = cfg.settings.clientTimeout;
+        InitialConnectTimeout = cfg.settings.clientTimeout;
+        LanServerMaxTickRate = cfg.settings.maxTickrate;
+        NetServerMaxTickRate = cfg.settings.maxTickrate;
+      };
+      "ConsoleVariables" = {
+        "wp.Runtime.EnableServerStreaming" = if cfg.settings.streaming then "1" else "0";
+      };
+      "Core.Log" = {
+        LogNet = "Error";
+        LogNetTraffic = "Warning";
+      };
+    };
+    Game = {
+      "/Script/Engine.GameSession" = {
+        ConnectionTimeout = cfg.settings.clientTimeout;
+        InitialConnectTimeout = cfg.settings.clientTimeout;
+        MaxPlayers = cfg.settings.maxPlayers;
+      };
+    };
+    GameUserSettings."/Script/Engine.GameSession" = {
+      MaxPlayers = cfg.settings.maxPlayers;
+    };
+  };
+
+  iniArgs = lib.flatten (
+    mapAttrsToList (
+      file:
+      mapAttrsToList (
+        section: mapAttrsToList (key: val: "-ini:${file}:[${section}]:${key}=${toString val}")
+      )
+    ) (lib.recursiveUpdate iniFromSettings cfg.iniSettings)
+  );
+
+  serverArgs = [
+    "-Port=${toString cfg.port}"
+    "-ReliablePort=${toString cfg.messagingPort}"
+    "-ExternalReliablePort=${toString cfg.messagingPort}"
+    "-multihome=${cfg.multihome}"
+  ]
+  ++ lib.optionals (!cfg.settings.seasonalEvents) [
+    "-DisableSeasonalEvents"
+  ]
+  ++ iniArgs;
 in
 {
   options.services.satisfactory = {
@@ -15,24 +74,41 @@ in
     package = lib.mkPackageOption pkgs "satisfactory-server" { };
 
     stateDir = lib.mkOption {
-      type = lib.types.path;
+      type = types.path;
       default = "/var/lib/satisfactory";
       description = "Directory to store the server state.";
     };
 
     user = lib.mkOption {
-      type = lib.types.str;
+      type = types.str;
       default = "satisfactory";
       description = "User to run the server as.";
     };
 
     port = lib.mkOption {
-      type = lib.types.port;
+      type = types.port;
       default = 7777;
       description = ''
-        Override the Game Port the server uses.
+        Override the game port the server uses.
         This is the primary port used to communicate game telemetry with the client.
-        If it is already in use, the server will step up to the next port until an available one is found.	
+        If it is already in use, the server will step up to the next port until an available one is found.
+      '';
+    };
+
+    listenAddr = lib.mkOption {
+      type = types.str;
+      description = ''
+        See https://satisfactory.wiki.gg/wiki/Dedicated_servers#Is_the_server_bound_to_the_correct_interface?
+        Defaults to `::`, which means the server will listen on all interfaces.
+      '';
+      default = "::";
+    };
+
+    messagingPort = lib.mkOption {
+      type = types.port;
+      default = 8888;
+      description = ''
+        Override the messaging port the server uses.
       '';
     };
 
@@ -43,16 +119,16 @@ in
     settings = lib.mkOption {
       description = "Satisfactory engine & game settings.";
       default = { };
-      type = lib.types.submodule {
+      type = types.submodule {
         options = {
           autosaveNumber = lib.mkOption {
             description = "Specifies the number of rotating autosaves to keep.";
-            type = lib.types.ints.positive;
+            type = types.ints.positive;
             default = 5;
           };
           clientTimeout = lib.mkOption {
             description = "Specifies the number of rotating autosaves to keep.";
-            type = lib.types.ints.positive;
+            type = types.ints.positive;
             default = 5;
           };
           streaming = lib.mkEnableOption "asset streaming" // {
@@ -60,22 +136,73 @@ in
           };
           maxObjects = lib.mkOption {
             description = "Specifies the maximum object limit for the server.";
-            type = lib.types.ints.positive;
+            type = types.ints.positive;
             default = 2162688;
           };
           maxTickrate = lib.mkOption {
             description = "Specifies the maximum tick rate for the server.";
-            type = lib.types.ints.positive;
+            type = types.ints.positive;
             default = 30;
           };
           maxPlayers = lib.mkOption {
             description = "Specifies the maximum number of players to allow on the server.";
-            type = lib.types.ints.positive;
+            type = types.ints.positive;
             default = 4;
           };
           seasonalEvents = lib.mkEnableOption "seasonal events, such as FICSMAS" // {
             default = true;
           };
+        };
+      };
+    };
+
+    iniSettings = lib.mkOption {
+      description = ''
+        Freeform type to pass arbitrary `-ini` options to the server.
+        See e.g. the [Satisfactory Wiki](https://satisfactory.wiki.gg/wiki/Multiplayer#Engine.ini)
+        for recommended config tweaks.
+      '';
+
+      type = types.submodule {
+        # generated via iniArgs
+        type = types.attrsOf (
+          types.attrsOf (
+            types.attrsOf (
+              types.enum [
+                types.str
+                types.number
+              ]
+            )
+          )
+        );
+      };
+
+      default = { };
+
+      example = {
+        Engine = {
+          "/Script/Engine.Player" = {
+            ConfiguredInternetSpeed = 104857600;
+            ConfiguredLanSpeed = 104857600;
+          };
+          "/Script/OnlineSubsystemUtils.IpNetDriver" = {
+            MaxClientRate = 104857600;
+            MaxInternetClientRate = 104857600;
+          };
+          "/Script/SocketSubsystemEpic.EpicNetDriver" = {
+            MaxClientRate = 104857600;
+            MaxInternetClientRate = 104857600;
+          };
+        };
+        Game."/Script/Engine.GameNetworkManager" = {
+          TotalNetBandwidth = 104857600;
+          MaxDynamicBandwidth = 104857600;
+          MinDynamicBandwidth = 10485760;
+        };
+        Scalability."NetworkQuality@3" = {
+          TotalNetBandwidth = 104857600;
+          MaxDynamicBandwidth = 104857600;
+          MinDynamicBandwidth = 10485760;
         };
       };
     };
@@ -103,43 +230,24 @@ in
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
 
-      preStart = ''
-        mkdir -p ${cfg.stateDir}/.config/Epic/FactoryGame
-      '';
-
       serviceConfig = {
         Type = "exec";
         User = cfg.user;
-        ExecStart = lib.escapeShellArgs (
-          [
-            (lib.getExe cfg.package)
-            "-ini:Engine:[/Script/Engine.Engine]:NetClientTicksPerSecond=${toString cfg.settings.maxTickrate}"
-            "-ini:Engine:[/Script/Engine.GarbageCollectionSettings]:gc.MaxObjectsInEditor=${toString cfg.settings.maxObjects}"
-            "-ini:Engine:[/Script/FactoryGame.FGSaveSession]:mNumRotatingAutosaves=${toString cfg.settings.autosaveNumber}"
-            "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:ConnectionTimeout=${toString cfg.settings.clientTimeout}"
-            "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:InitialConnectTimeout=${toString cfg.settings.clientTimeout}"
-            "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:LanServerMaxTickRate=${toString cfg.settings.maxTickrate}"
-            "-ini:Engine:[/Script/OnlineSubsystemUtils.IpNetDriver]:NetServerMaxTickRate=${toString cfg.settings.maxTickrate}"
-            "-ini:Engine:[ConsoleVariables]:wp.Runtime.EnableServerStreaming=${
-              if cfg.settings.streaming then "1" else "0"
-            }"
-            "-ini:Engine:[Core.Log]:LogNet=Error"
-            "-ini:Engine:[Core.Log]:LogNetTraffic=Warning"
-            "-ini:Game:[/Script/Engine.GameSession]:ConnectionTimeout=${toString cfg.settings.clientTimeout}"
-            "-ini:Game:[/Script/Engine.GameSession]:InitialConnectTimeout=${toString cfg.settings.clientTimeout}"
-            "-ini:Game:[/Script/Engine.GameSession]:MaxPlayers=${toString cfg.settings.maxPlayers}"
-            "-ini:GameUserSettings:[/Script/Engine.GameSession]:MaxPlayers=${toString cfg.settings.maxPlayers}"
-            "-Port=${builtins.toString cfg.port}"
-            "-DisablePacketRouting"
-          ]
-          ++ lib.optionals (!cfg.settings.seasonalEvents) [ "-DisableSeasonalEvents" ]
-        );
+        ExecStart = pkgs.writeShellScriptBin "satisfactory-server" /* bash */ ''
+          mkdir -p "${cfg.stateDir}/.config/Epic/FactoryGame"
+          ${lib.getExe cfg.package} ${lib.escapeShellArgs serverArgs}
+        '';
       };
     };
 
     networking.firewall = lib.mkIf cfg.openFirewall {
-      allowedTCPPorts = [ cfg.port ];
-      allowedUDPPorts = [ cfg.port ];
+      allowedTCPPorts = [
+        cfg.port
+        cfg.messagingPort
+      ];
+      allowedUDPPorts = [
+        cfg.port
+      ];
     };
   };
 }
